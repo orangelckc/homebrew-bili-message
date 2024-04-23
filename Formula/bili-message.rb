29bdeca46c51e74d50e868b8e2bfcaffc743b205
orangelckc <276126767@qq.com>
2024-04-23 13:11:29 +0200

    chore: 更新Formula

--- Formula/bili-message.rb
+++ Formula/bili-message.rb
@@ -3,7 +3,7 @@
 class BiliMessage < Formula
   desc "bilibili简单弹幕机"
   homepage "https://github.com/orangelckc/bili-message"
-  url "https://github.com/orangelckc/bili-message/releases/download/v1.2.8/bilibili._x64.app.tar.gz"
+  url "https://github.com/orangelckc/bili-message/releases/latest/download/bilibili._x64.app.tar.gz"
   sha256 "6cc67dda49493cf4515c1657b7c60d96d22e486de801ad6010164f95d663cc48"
   license "MIT"
   version "1.2.8"
@@ -11,6 +11,6 @@ class BiliMessage < Formula
   # depends_on "cmake" => :build
 
   def install
-    bin.install "6cc67dda49493cf4515c1657b7c60d96d22e486de801ad6010164f95d663cc48--bilibili._x64.app"
+    bin.install "bilibili._x64.app"
   end
 end
