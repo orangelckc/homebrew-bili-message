d5443789d561c2e425fb233f3643d91c7e1119cb
orangelckc <276126767@qq.com>
2024-04-23 13:19:57 +0200

    chore: 使用casks脚本

---
+++ Casks/bili-message.rb
@@ -0,0 +1,10 @@
+cask "bili-message" do
+  version "1.2.8"
+  url "https://github.com/orangelckc/bili-message/releases/latest/download/bilibili._x64.app.tar.gz"
+  sha256 "6cc67dda49493cf4515c1657b7c60d96d22e486de801ad6010164f95d663cc48"
+  name "bili-message"
+  desc "bilibili简单弹幕机"
+  homepage "https://github.com/orangelckc/bili-message"
+
+  app "bilibili弹幕机.app"
+end
